38bbbb8413fbd20946d5b785373e10413013f177
Mayank Kumar <mk4664348@gmail.com>
2026-08-22 20:46:36 +0530

    Image renaming

--- constants/images.ts
+++ constants/images.ts
@@ -1,6 +1,6 @@
 export const BreathingImages = {
-  box: require('@/assets/images/breathing/box-breathing.png'),
-  sigh: require('@/assets/images/breathing/box-breathing.png'),
-  fourSevenEight: require('@/assets/images/breathing/box-breathing.png'),
-  phoneBg: require('@/assets/images/breathing/phone-bg.png'),
+  box: require('@/assets/images/breathing/boxbreathing.png'),
+  sigh: require('@/assets/images/breathing/sighbreathing.png'),
+  fourSevenEight: require('@/assets/images/breathing/fourseveneight.png'),
+  phoneBg: require('@/assets/images/breathing/phonebg.png'),
 };
