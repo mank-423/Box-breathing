8b35803664f47b0b644344702ed9b8e66ef89ff7
Mayank Kumar <mk4664348@gmail.com>
2026-09-05 21:10:00 +0530

    Widget imporvement and image change

--- constants/images.ts
+++ constants/images.ts
@@ -1,6 +1,3 @@
 export const BreathingImages = {
-  box: require('@/assets/images/breathing/boxbreathing.png'),
-  sigh: require('@/assets/images/breathing/sighbreathing.png'),
-  fourSevenEight: require('@/assets/images/breathing/fourseveneight.png'),
   phoneBg: require('@/assets/images/breathing/phonebg.png'),
 };
